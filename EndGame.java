83139d8ac2118770cc8f8223d2dfc719afa5078f
Patrick Riley <rileyp@bishopireton.org>
2017-06-04 21:14:28 -0400

    Add files via upload

--- EndGame.java
+++ EndGame.java
@@ -2,9 +2,26 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class EndGame extends SetupBoard{
-
+   
+   public JButton reset; 
+   
+   
+   public EndGame(){
+      reset = new JButton("Reset");     
+      reset.addActionListener(new ResetButtonListener());
+      add(reset);
+      
+      JPanel panel = new JPanel(new BorderLayout());
+      panel.add(this, BorderLayout.WEST);
+   }
+   
+   
+   
    public void paintComponent(Graphics g){
       super.paintComponent(g);  
       paintStuff(g);
@@ -19,14 +36,27 @@ public class EndGame extends SetupBoard{
          winGame(g);
         
    }
-
+   
+   
+   
+   private class ResetButtonListener
+   implements ActionListener
+   {
+      public void actionPerformed(ActionEvent e) 
+      { 
+        
+      }
+   } 
+   
+   
+   
    public void loseGame(Graphics g){
       Bricks.killBricks();        
       g.setColor(new Color(195, 195, 195));   
       g.fillRect(0,0,700,700);
       g.setColor(Color.BLACK);
       g.setFont(new Font("Comic Sans MS", Font.PLAIN, 48));
-      g.drawString(GameConstants.lost, GameConstants.WINDOW_SIZE/4, GameConstants.WINDOW_SIZE/4);
+      g.drawString(GameConstants.lost, GameConstants.WINDOW_SIZE/7, GameConstants.WINDOW_SIZE/4);
    }
    
    public void winGame(Graphics g){
@@ -34,7 +64,7 @@ public class EndGame extends SetupBoard{
       g.fillRect(0,0,700,700);
       g.setColor(Color.BLACK);
       g.setFont(new Font("Comic Sans MS", Font.PLAIN, 48));
-      g.drawString(GameConstants.won, GameConstants.WINDOW_SIZE/4, GameConstants.WINDOW_SIZE/4);
+      g.drawString(GameConstants.won, GameConstants.WINDOW_SIZE/7, GameConstants.WINDOW_SIZE/4);
       
    }
 
